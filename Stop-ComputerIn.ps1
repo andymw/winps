a522e2076db31e1e52dbf1e3301fd44f76fb5cad
andymw <andymw91@gmail.com>
2015-05-28 01:56:52 -0400

    more slight aesthetic stuff

--- Stop-ComputerIn.ps1
+++ Stop-ComputerIn.ps1
@@ -1,26 +1,26 @@
 <#
 .SYNOPSIS
-Stops computer, given int parameter in minutes (Default 10)
+Stops or restarts computer, given int parameter in minutes (default 10)
 .DESCRIPTION
+Stops or restarts computer, given int parameter in minutes (default 10)
+Minute range allowed from 1 to 525599 (1 minute before 1 year).
 .NOTES
 Windows PowerShell 3.0 script
 .EXAMPLE
-Stop-ComputerIn 15
-Creates a scheduled task to run a shutdown in 15 minutes and registers it.
+Stop-ComputerIn -r 15
+Creates a scheduled task to run a reboot in 15 minutes and registers it.
 #>
 [CmdletBinding()]
 param (
-    [Parameter(Position=0)][ValidateRange(1,1439)][int]$Minutes = 10,
+    [Parameter(Position=0)][ValidateRange(1,525599)][int]$Minutes = 10,
     [Parameter()][switch]$Restart,
     [Parameter()][switch]$Help
 )
 
-if ($Help) {
-    Get-Help $MyInvocation.MyCommand.path; exit
-}
+if ($Help) { Get-Help $MyInvocation.MyCommand.path; exit }
 
 if ($Restart) { $cmdop = 'Restart-Computer' }
-else          { $cmdop = 'Stop-Computer'    }
+else          { $cmdop =    'Stop-Computer' }
 
 $args = "-NoProfile -WindowStyle Hidden -command $cmdop"
 
